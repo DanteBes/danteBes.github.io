e5168d5d7acf30378b887af0818f74d92d015520
Your Name <you@example.com>
2026-04-24 17:43:44 +0000

    feat: add BEM classes to Footer.jsx (footer, footer__bar_link, item, developer, effect)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,19 @@
 function Footer() {
-  return <footer>© 2025 Ярослав Бесфалин</footer>;
+  return 
+    <footer classmame="footer">
+        <div classmame="footer_bar_link">
+          <a href="" classmame="footer_bar_link-item footer__bar_link-item--developer  footer__bar_link-item--developer_efect">
+            Главный разработчик
+          </a>
+          <a href="" classmame="footer__bar_link-item  footer__bar_link-item--developer  footer__bar_link-item--developer_efevt">
+            Самый лучший разработчик
+          </a>
+        </div>
+        <div>
+          <span>
+            © 2025 Ярослав Бесфалин
+          </span>
+        </div>
+      </footer>
 }
 export default Footer;
