1fc0b17f1e8759cd600f11a3dd5bf2ba19a52a90
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-05-05 14:37:44 +0000

    fix css for footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,39 @@
-import "./Footer.module.css";
+import styles from "./Footer.module.css";
 
 export function Footer() {
   return (
-    <footer className="footer">
-        <div className="footer_bar_link">
-          <a href="#" className="footer_bar_link-item footer__bar_link-item--developer  footer__bar_link-item--developer_effect">
-            Главный разработчик
-          </a>
-          <a href="#" className="footer__bar_link-item  footer__bar_link-item--developer  footer__bar_link-item--developer_effect">
-            Самый лучший разработчик
-          </a>
-        </div>
-        <div className="footer__WaterMark">
-          <span className="footer__WaterMark-text">
-            © 2025 Ярослав Бесфалин
-          </span>
-        </div>
-      </footer>
-  )
+    <footer className={styles.footer}>
+      <div className={styles["footer__bar_link"]}>
+        <a
+          href="#"
+          className={
+            styles["footer__bar_link-item"] +
+            " " +
+            styles["footer__bar_link-item--developer"] +
+            " " +
+            styles["footer__bar_link-item--developer_effect"]
+          }
+        >
+          Главный разработчик
+        </a>
+        <a
+          href="#"
+          className={
+            styles["footer__bar_link-item"] +
+            " " +
+            styles["footer__bar_link-item--developer"] +
+            " " +
+            styles["footer__bar_link-item--developer_effect"]
+          }
+        >
+          Самый лучший разработчик
+        </a>
+      </div>
+      <div className={styles["footer__WaterMark"]}>
+        <span className={styles["footer__WaterMark-text"]}>
+          © 2025 Ярослав Бесфалин
+        </span>
+      </div>
+    </footer>
+  );
 }
