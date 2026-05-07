dab6cd3e3cf7aabeb2a6a1288dda24ec6bce0352
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-05-07 17:26:46 +0000

    Замена статичного подзаголовка на случайный текст

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -2,7 +2,16 @@ import { Link } from "react-router-dom";
 import styles from "./AppHeader.module.css";
 import ThemeSwitcher from "../theme-switcher/ThemeSwitcher";
 
+const textArray = [
+	"hello",
+	"my",
+	"world"
+];
+
 function AppHeader() {
+  const n = Math.floor(Math.random() * textArray.length);
+  const text = textArray[n]
+
   return (
     <header className={styles.header}>
       <div className={styles.header__top}>
@@ -11,7 +20,7 @@ function AppHeader() {
         </Link>
         <ThemeSwitcher />
       </div>
-      <h2 className={styles.header__subtitle}>Любитель умных штук;)</h2>
+      <h2 className={styles.header__subtitle}>{text}</h2>
     </header>
   );
 }
