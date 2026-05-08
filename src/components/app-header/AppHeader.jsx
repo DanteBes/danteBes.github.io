b3226125964e5486e7758b6290da72c7d80a4e9a
Dante Besfalin? <befalinfreelancer@gmail.com>
2026-05-08 11:16:36 +0000

    feat: implemented interactive/live logo in AppHeader and added content below it

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -2,21 +2,37 @@ import { Link } from "react-router-dom";
 import styles from "./AppHeader.module.css";
 import ThemeSwitcher from "../theme-switcher/ThemeSwitcher";
 
+const headerLogo = [
+  "Ярослав Бесфалин",
+  "Ярослав",
+  "Dante Besfalin",
+  "Dante",
+  "Besfalin",
+  "Yaroslav"
+];
+
 const textArray = [
 	"hello",
 	"my",
-	"world"
+	"world",
+  "👋👋👋",
+  "🤪🤪🤪",
+  "😮😮😮"
 ];
 
 function AppHeader() {
   const n = Math.floor(Math.random() * textArray.length);
   const text = textArray[n]
 
+  //Анимированный логотип//
+  const nLogo = Math.floor(Math.random() * headerLogo.length);
+  const headerRandom = headerLogo[nLogo]
+
   return (
     <header className={styles.header}>
       <div className={styles.header__top}>
         <Link className={styles.header__titleLink} to="/">
-          <h1 className={styles.title}>Ярослав Бесфалин</h1>
+          <h1 className={styles.title}>{headerRandom}</h1>
         </Link>
         <ThemeSwitcher />
       </div>
